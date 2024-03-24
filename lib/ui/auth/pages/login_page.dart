b74334f22bc3d60061b420dbedef398d7c27ab3b
muflikhandimasd <muflikhan.dimas@gmail.com>
2024-03-24 14:42:01 +0700

    BUILD WHEN AND LISTEN WHEN AUTH

--- lib/ui/auth/pages/login_page.dart
+++ lib/ui/auth/pages/login_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:training_flutter/blocs/auth/auth_cubit.dart';
 import 'package:training_flutter/core/enum/api_status.dart';
+import 'package:training_flutter/core/helpers/show_error_message.dart';
 import 'package:training_flutter/core/routes/app_path.dart';
 
 class LoginPage extends StatelessWidget {
@@ -13,67 +14,80 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                TextFormField(
-                  controller: _emailController,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Email tidak valid';
-                    }
+    return BlocListener<AuthCubit, AuthState>(
+      listenWhen: (previous, current) => previous.loginStatus != current.loginStatus,
+      listener: (context, state) {
+        if (state.loginStatus.isError) {
+         showErrorMessage(context, state.message);
+        }
 
-                    return null;
-                  },
-                  decoration: const InputDecoration(
-                    labelText: 'Email',
+        if (state.loginStatus.isSuccess) {
+          Navigator.pushNamedAndRemoveUntil(context, AppPath.posts, (route) => false);
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Login'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: _emailController,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Email tidak valid';
+                      }
+
+                      return null;
+                    },
+                    decoration: const InputDecoration(
+                      labelText: 'Email',
+                    ),
                   ),
-                ),
-                TextFormField(
-                  controller: _passwordController,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Password tidak valid';
-                    }
-                    return null;
-                  },
-                  decoration: const InputDecoration(
-                    labelText: 'Password',
+                  TextFormField(
+                    controller: _passwordController,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Password tidak valid';
+                      }
+                      return null;
+                    },
+                    decoration: const InputDecoration(
+                      labelText: 'Password',
+                    ),
                   ),
-                ),
-                BlocBuilder<AuthCubit, AuthState>(
-                  builder: (context, state) {
-                    return ElevatedButton(
-                      onPressed: () {
-                        if (state.loginStatus.isLoading) return;
-                        if (_formKey.currentState!.validate()) {
-                          context.read<AuthCubit>().login(
-                                email: _emailController.text,
-                                password: _passwordController.text,
-                              );
-                        }
-                      },
-                      child: state.loginStatus.isLoading
-                          ? const CircularProgressIndicator()
-                          : const Text('Login'),
-                    );
-                  },
-                ),
-                TextButton(
-                onPressed: () {
-                  Navigator.pushReplacementNamed(context, AppPath.register);
-                },
-                child: const Text('Belum punya akun? Register disini'),
-              ),
-              ],
-            )),
+                  BlocBuilder<AuthCubit, AuthState>(
+                    buildWhen: (previous, current) => previous.loginStatus != current.loginStatus,
+                    builder: (context, state) {
+                      return ElevatedButton(
+                        onPressed: () {
+                          if (state.loginStatus.isLoading) return;
+                          if (_formKey.currentState!.validate()) {
+                            context.read<AuthCubit>().login(
+                              email: _emailController.text,
+                              password: _passwordController.text,
+                            );
+                          }
+                        },
+                        child: state.loginStatus.isLoading
+                            ? const CircularProgressIndicator()
+                            : const Text('Login'),
+                      );
+                    },
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pushReplacementNamed(context, AppPath.register);
+                    },
+                    child: const Text('Belum punya akun? Register disini'),
+                  ),
+                ],
+              )),
+        ),
       ),
     );
   }
--- lib/ui/auth/pages/register_page.dart
+++ lib/ui/auth/pages/register_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
 import 'package:training_flutter/blocs/auth/auth_cubit.dart';
 import 'package:training_flutter/core/enum/api_status.dart';
+import 'package:training_flutter/core/helpers/show_error_message.dart';
 import 'package:training_flutter/core/routes/app_path.dart';
 
 class RegisterPage extends StatelessWidget {
@@ -15,84 +16,97 @@ class RegisterPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Register'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              TextFormField(
-                controller: _nameController,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Nama tidak valid';
-                  }
-                  return null;
-                },
-                decoration: const InputDecoration(
-                  labelText: 'Nama',
-                ),
-              ),
-              TextFormField(
-                controller: _emailController,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Email tidak valid';
-                  }
-                  return null;
-                },
-                decoration: const InputDecoration(
-                  labelText: 'Email',
-                ),
-              ),
-              TextFormField(
-                controller: _passwordController,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Password tidak valid';
-                  }
-                  return null;
-                },
-                decoration: const InputDecoration(
-                  labelText: 'Password',
-                ),
-              ),
-              BlocBuilder<AuthCubit, AuthState>(
-                builder: (context, state) {
-                  return ElevatedButton(
-                    onPressed: () {
-                      if (state
-                          .registerStatus
-                          .isLoading) return;
-                      if (_formKey.currentState!.validate()) {
-                        context.read<AuthCubit>().register(
-                              name: _nameController.text,
-                              email: _emailController.text,
-                              password: _passwordController.text,
-                            );
-                      }
-                    },
-                    child:   state
-                            .registerStatus
-                            .isLoading
-                        ? const CircularProgressIndicator()
-                        : const Text('Register'),
-                  );
-                },
-              ),
+    return BlocListener<AuthCubit, AuthState>(
+      listenWhen: (previous, current) => previous.registerStatus != current.registerStatus,
+      listener: (context, state) {
+        if (state.registerStatus.isError) {
+          showErrorMessage(context, state.message);
+        }
 
-              // ALREADY HAVE AN ACCOUNT
-              TextButton(
-                onPressed: () {
-                  Navigator.pushReplacementNamed(context, AppPath.login);
-                },
-                child: const Text('Sudah punya akun? Login'),
-              ),
-            ],
+        if (state.registerStatus.isSuccess) {
+          Navigator.pushNamedAndRemoveUntil(context, AppPath.posts, (route) => false);
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Register'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: _nameController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Nama tidak valid';
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(
+                    labelText: 'Nama',
+                  ),
+                ),
+                TextFormField(
+                  controller: _emailController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Email tidak valid';
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(
+                    labelText: 'Email',
+                  ),
+                ),
+                TextFormField(
+                  controller: _passwordController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Password tidak valid';
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(
+                    labelText: 'Password',
+                  ),
+                ),
+                BlocBuilder<AuthCubit, AuthState>(
+                  buildWhen: (previous, current) => previous.registerStatus != current.registerStatus,
+                  builder: (context, state) {
+                    return ElevatedButton(
+                      onPressed: () {
+                        if (state
+                            .registerStatus
+                            .isLoading) return;
+                        if (_formKey.currentState!.validate()) {
+                          context.read<AuthCubit>().register(
+                            name: _nameController.text,
+                            email: _emailController.text,
+                            password: _passwordController.text,
+                          );
+                        }
+                      },
+                      child: state
+                          .registerStatus
+                          .isLoading
+                          ? const CircularProgressIndicator()
+                          : const Text('Register'),
+                    );
+                  },
+                ),
+
+                // ALREADY HAVE AN ACCOUNT
+                TextButton(
+                  onPressed: () {
+                    Navigator.pushReplacementNamed(context, AppPath.login);
+                  },
+                  child: const Text('Sudah punya akun? Login'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
